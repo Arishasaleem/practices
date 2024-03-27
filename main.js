5142c69c717f78da2f5c26469d694656117a5f6a
Arishasaleem <159950170+Arishasaleem@users.noreply.github.com>
2024-03-27 22:27:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//class assignment
+// function
+//peramter/argument
+//returm
+var someNames = "fruitName";
+function letSomeNames() {
+    var someNames = "mango";
+    console.log(someNames);
+}
+letSomeNames();
+var shoesName = "heels";
+function letShoesName() {
+    var shoesName = "heels";
+    console.log(shoesName);
+}
+letShoesName();
+//solve the formula
+//velocity = s/t
+function findVelocity() {
+    var s = 24;
+    var t = 55;
+    var velocity = s / t;
+    console.log(velocity);
+}
+findVelocity();
---
+++ main.ts
@@ -0,0 +1,32 @@
+//class assignment
+// function
+//peramter/argument
+//returm
+
+let someNames: string="fruitName";
+
+function letSomeNames() {
+  let someNames = "mango";
+   console.log(someNames);
+
+}
+  letSomeNames();   
+
+let shoesName: string = "heels";
+function letShoesName(){
+  let shoesName = "heels";
+  console.log(shoesName);
+}
+
+letShoesName()
+
+//solve the formula
+//velocity = s/t
+function findVelocity(){
+  let s = 24;
+  let t = 55;
+  let velocity = s / t
+  console.log(velocity);
+
+}
+findVelocity();
